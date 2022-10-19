121c95d351ed0bf54e3a9d712f671eb3d2859793
alafsasa <alafsasa@gmail.com>
2022-10-19 15:44:53 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Student, AppName, Level } from './interfaces';
+import { studentList, courseList } from './data';
+import { DisplayData } from './DisplayData';
+import { GenericSort } from './GenericSort';
 
-function App() {
+function App({ name }: AppName) {
+  const [student, setStudent] = useState<Student | null>(null);
+  const [list, setList] = useState(studentList);
+  const addStudent = () => {
+    setStudent({
+      name: 'George',
+      surname: 'Limo',
+      age: 20,
+      address: {
+        houseNo: 1234,
+        street: 'habor',
+        town: 'Eldoret'
+      },
+      level: "Undergraduate"
+    })
+  }
+  const sortData = () => {
+    GenericSort(studentList, "name");
+    setList([...studentList]);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Hello World!</h4>
+        <h1>{name}</h1>
+        <p><b>{student?.name} {student?.surname}</b></p>
+        {
+          student?.level === Level.Undergraduate && <p><b>Age: {student.age}</b></p>
+        }
+        <button onClick={addStudent}>Add Student</button>
+        <br />
+        <button onClick={sortData}>Sort Data</button>
+        <div>
+          <DisplayData items={list} />
+        </div>
+        <h3>List of Courses</h3>
+        <div>
+          <DisplayData items={courseList} />
+        </div>
       </header>
     </div>
   );
---
+++ src/data.ts
@@ -0,0 +1,14 @@
+export const studentList = [
+    {id: 1, name: 'Antony', surname: 'Ndungu', level: 'Undergraduate', age: 20},
+    {id: 2, name: 'Chanell', surname: 'John', level: 'Postgraduate', age: 50},
+    {id: 3, name: 'Ian', surname: 'Smith', level: 'Undergraduate', age: 46},
+    {id: 4, name: 'Michael', surname: 'Starke', level: 'Postgraduate', age: 64},
+    {id: 5, name: 'Chris', surname: 'De Kock', level: 'Undergraduate', age: 18},
+];
+
+export const courseList = [
+    {id: 1, code: 'A141', name: 'Algorithms Analysis', description: 'Analysis & Design'},
+    {id: 2, code: 'BN445', name: 'Computer Architecture I', description: 'Computer Architecture'},
+    {id: 3, code: 'P888', name: 'Operations Research', description: 'Maths - Operations Research'},
+    {id: 4, code: 'Z9989', name: 'Discrete Maths', description: 'Discrete Mathematics'},
+];
